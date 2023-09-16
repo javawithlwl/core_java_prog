b20052e5e79f82073599a41d4d6da56d2ef8f3fc
javawithlwl <javawithlwl@gmail.com>
2023-09-16 07:08:54 +0530

    Fixed formatting issues

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -2,13 +2,13 @@ public class SwapTwoNumbers{
 
 	public static void main(String[] args){
 	
-		int a =10;
-		int b =20;
+		int a = 10;
+		int b = 20;
 		System.out.println("Before swap a = "+a+" b = " + b);
 		int temp = a;
-		a=b;
-		b=temp;
-		System.out.println("After swap a = "+a+" b = " + b);
+		a = b;
+		b = temp;
+		System.out.println("After swap  a = "+a+" b = " + b);
 	
 	}
 
