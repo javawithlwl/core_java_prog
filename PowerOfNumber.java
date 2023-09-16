8185d992201202704e58460445094ce4fce63aac
javawithlwl <javawithlwl@gmail.com>
2023-09-16 06:11:53 +0530

    Core Java Examples

---
+++ PowerOfNumber.java
@@ -0,0 +1,9 @@
+public class PowerOfNumber{
+	
+		public static void main(String[] args){
+			int m = 10;
+			int n = 3;
+        	double res = Math.pow(m,n);
+			System.out.println(m+" ^ " +n +" is "+res);
+		}
+}
